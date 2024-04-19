6386579df3b58662b08a63869cd6354b5abf6f4f
Nella <nellasoudunsaari@gmail.com>
2024-04-19 14:26:10 +0300

    Lisätty tasonvalinta, itse tasosta puuttuu vielä pause menu ja kaikesta puuttuu oikeat grafiikat

--- Assets/Koodi/MainMenu.cs
+++ Assets/Koodi/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("TestiKenttä");
+        SceneManager.LoadScene("TasonValinta");
     }
 
     public void QuitGame()
